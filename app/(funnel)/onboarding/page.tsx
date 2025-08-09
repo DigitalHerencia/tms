f7db8fb1d9b166f504d922c9d19855b98114dbdd
Ivan Roman <DigitalHerencia@Outlook.com>
2025-08-09 07:10:47 -0600

    Delete app/(funnel)/onboarding directory

--- app/(funnel)/onboarding/page.tsx
+++
@@ -1,9 +0,0 @@
-import { OnboardingWizardFeature } from '@/features/admin/OnboardingWizardFeature';
-
-export default function AdminOnboardingPage() {
-  return (
-    <div className="container mx-auto py-10">
-      <OnboardingWizardFeature />
-    </div>
-  );
-}
